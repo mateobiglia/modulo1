aee36a4534b312bdb26026954963a9a6068142f6
mateobiglia <104086241+mateobiglia@users.noreply.github.com>
2022-05-23 16:54:24 -0300

    homework1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,11 +2,22 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let resultado = 0;
+  for(let i = 0; i < num.length; i++){
+    resultado += num[i] * Math.pow(2, num.length - 1 -i);
 
+  }
+ return resultado;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let array = [];
+  while(num > 0){
+    array.unshift(num % 2);
+    num = Math.floor(num / 2);
+  }
+  return array.join("");
 
 }
 
